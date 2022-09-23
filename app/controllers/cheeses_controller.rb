be2e55d3442d285f50640339d56f09dadedf9554
Patrick <ruomungai@gmail.com>
2022-09-23 13:06:32 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -8,8 +8,13 @@ class CheesesController < ApplicationController
 
   # GET /cheeses/:id
   def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+  cheese = Cheese.find_by(id: params[:id])
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    # status: :not_found will produce a 404 status code
+    render json: { error: 'Cheese not found' }, status: :not_found
   end
+end
 
 end
